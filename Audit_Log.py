5de6554caac506c0d3883061670c80641796a010
Gabriel Tinsley <gabrieltinsley@u.boisestate.edu>
2025-04-02 13:02:19 -0600

    Started

--- Audit_Log.py
+++ Audit_Log.py
@@ -1,1 +1,8 @@
 # Team #2 Audit Log for Part 2 of Assignment 4
+
+# Write tests
+
+# Allow privileged users to write events
+# def add_log_event(user_id: str, event_data: dict) -> bool:
+# Prevent unauthorized access
+
