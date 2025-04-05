cf30ead0c8cc18bfd0561b9478f07f51ba1512ba
Gabriel Tinsley <gabrieltinsley@u.boisestate.edu>
2025-04-05 17:30:25 -0600

    Part 2 is complete.

--- Audit_Log.py
+++ Audit_Log.py
@@ -1,9 +1,13 @@
 # Team #2 Audit Log for Part 2 of Assignment 4
-# Authors: Gabriel Tinsley
+# Authors: Gabriel Tinsley, Sariah Bjornn, Eben Meyer, Chase Stombaugh
 
-# Write tests
-
-# Allow privileged users to write events
-# def add_log_event(user_id: str, event_data: dict) -> bool:
-# Prevent unauthorized access
+# Allow privileged users to write events while preventing unauthorized access and handling errors
+# Input parameters:
+#   user_id: String object that specifies the username
+#   event_id: Dictionary that specifies the event data
+# Exceptions: If user_id is NOT a string or event_data is NOT a dictionary
+# Security checks: Returns true if user can write events
+#                  Returns false if user can NOT write events
+def add_log_event(user_id: str, event_data: dict) -> bool:
+    return
 
