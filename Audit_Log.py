5ef2f8ee4c1fbeb59324c83e5dbf5a839e85b114
Gabriel Tinsley <gabrieltinsley@u.boisestate.edu>
2025-04-17 02:22:55 -0600

    Audit log text file added and tests for function added

--- Audit_Log.py
+++ Audit_Log.py
@@ -1,20 +1,6 @@
 # Team #2 Audit Log for Part 2 of Assignment 4
 # Authors: Gabriel Tinsley, Sariah Bjornn, Eben Meyer, Chase Stombaugh
 
-# Allow privileged users to write events while preventing unauthorized access and handling errors
-# Input parameters:
-#   user_id: String object that specifies the username
-#   event_id: Dictionary that specifies the event data
-# Exceptions: If user_id is NOT a string or event_data is NOT a dictionary
-# Security checks: Returns true if user can write events
-#                  Returns false if user can NOT write events
-# def add_log_event(user_id: str, event_data: dict) -> bool:
-    # return
-
-
-# Team #2 Audit Log for Part 2 of Assignment 4
-# Authors: Gabriel Tinsley, Sariah Bjornn, Eben Meyer, Chase Stombaugh
-
 # User roles
 admin_users = ["admin1", "admin2"]
 
@@ -44,3 +30,12 @@ def add_log_event(user_id: str, event_data: dict) -> bool:
     except Exception as e: 
         print(f"There was an error while writing to log: {e}")
         return False
+
+    
+# Tests for code
+add_log_event("admin1", {"action": "login", "status": "success"}) # pass admin user
+add_log_event("user123", {"action": "logout", "status": "success"}) # fail not admin user
+add_log_event(123, {"action": "delete", "target": "record"}) # fail invalid user ID
+
+
+    
